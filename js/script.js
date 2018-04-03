38e8e0aabf06b05eb934a9246f3719e060059cb5
Sébastien PREVOST <sebastien.prevost31@gmail.com>
2018-04-03 22:09:34 +0200

    Utilisation boostrap

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
-var previous = document.getElementsByClassName('previous')[0].addEventListener('click', function (event) {
+var previous = document.getElementsByClassName('previous')[0].addEventListener('click', function(event) {
     console.log('Click sur le précédent');
 });
 
-var next = document.getElementsByClassName('next')[0].addEventListener('click', function (event) {
+var next = document.getElementsByClassName('next')[0].addEventListener('click', function(event) {
     console.log('Click sur le précédent');
 });
 
+var timerProgress = undefined;
+
+var initTimer = function(secondes) {
+    var progress = document.getElementById('pbar');
+
+    var i = 0;
+
+    timerProgress = setInterval(function() {
+        i = i + 1;
+        progress.style.width = i + '%';
+        progress.innerText = i + '%';
+    }, 1000);
+
+}
