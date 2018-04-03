1b11d377feb27af72fbd4618028f5249bb8ce373
Sébastien Prévost <seprevost@airfrance.fr>
2018-04-03 14:58:22 +0200

    Mise en place

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var previous = document.getElementsByClassName('previous')[0].addEventListener('click', function (event) {
+    console.log('Click sur le précédent');
+});
+
+var next = document.getElementsByClassName('next')[0].addEventListener('click', function (event) {
+    console.log('Click sur le précédent');
+});
+
