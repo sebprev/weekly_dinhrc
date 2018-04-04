76a66e6b614625099a5028ff24133da8e7d21a9b
Sébastien Prévost <seprevost@airfrance.fr>
2018-04-04 10:58:08 +0200

    Ajout des données

---
+++ js/data.js
@@ -0,0 +1,97 @@
+const milweb = {
+    projet: 'MILWEB',
+    personnes: 'Joël',
+    duree: 240
+};
+
+const sirocco = {
+    projet: 'SIROCCO',
+    personnes: 'Phu Quang',
+    duree: 240
+};
+
+const siroccolight = {
+    projet: 'SIROCCO LIGHT',
+    personnes: 'Philippe',
+    duree: 120
+};
+
+const atcweb = {
+    projet: 'ATCWEB',
+    personnes: 'Philippe',
+    duree: 120
+};
+
+const occtet = {
+    projet: 'OCCTET',
+    personnes: 'Philippe',
+    duree: 60
+};
+
+const tfm = {
+    projet: 'TFM',
+    personnes: 'Laurent',
+    duree: 180
+};
+
+const ishare = {
+    projet: 'ISHARE',
+    personnes: 'Laurent',
+    duree: 120
+};
+
+const ptrakl = {
+    projet: 'PTRAKL',
+    personnes: 'Laurent',
+    duree: 60
+};
+
+const amigo = {
+    projet: 'AMIGO',
+    personnes: 'Corinne',
+    duree: 120
+};
+
+const vidcco = {
+    projet: 'VIDCCO',
+    personnes: 'Corinne',
+    duree: 60
+};
+
+const encours = {
+    projet: 'Projets en cours',
+    personnes: '(Step, Mohican, Boost)',
+    duree: 180
+};
+
+const archi = {
+    projet: "Sujets d'architecture",
+    personnes: 'Jean Marc',
+    duree: 240
+};
+
+const technique = {
+    projet: 'Sujets techniques',
+    personnes: 'Philippe, Laurent',
+    duree: 240
+};
+
+const infos = {
+    projet: "Infos générales",
+    personnes: 'Nathalie',
+    duree: 240
+};
+
+const demo = {
+    projet: 'Démo de la semaine',
+    personnes: '???',
+    duree: 300
+};
+
+const nexttk = {
+    projet: 'Prochain Time Keeper',
+    personnes: '',
+    duree: 0
+};
+
+const data = [milweb, sirocco, siroccolight, atcweb, occtet, tfm, ishare, ptrakl, amigo, vidcco, encours, archi, technique, infos, demo, nexttk];
--- js/script.js
+++ js/script.js
@@ -1,22 +1,83 @@
-var previous = document.getElementsByClassName('previous')[0].addEventListener('click', function(event) {
-    console.log('Click sur le précédent');
+const projetElem = document.getElementById('projet');
+const personnesElem = document.getElementById('personnes');
+const progress = document.getElementById('pbar');
+const time = document.getElementById('time');
+const imgLeft = document.getElementById('imgLeft');
+const imgRight = document.getElementById('imgRight');
+
+const previous = document.getElementsByClassName('previous')[0].addEventListener('click', function(event) {
+    if (currentIndex > 0) {
+        currentIndex = currentIndex - 1;
+        start();
+    }
 });
 
-var next = document.getElementsByClassName('next')[0].addEventListener('click', function(event) {
-    console.log('Click sur le précédent');
+const next = document.getElementsByClassName('next')[0].addEventListener('click', function(event) {
+    if ((currentIndex + 1) < data.length) {
+        currentIndex = currentIndex + 1;
+        start();
+    }
 });
 
-var timerProgress = undefined;
+let timerProgress = undefined;
 
-var initTimer = function(secondes) {
-    var progress = document.getElementById('pbar');
+let currentIndex = 0;
 
-    var i = 0;
-
-    timerProgress = setInterval(function() {
-        i = i + 1;
-        progress.style.width = i + '%';
-        progress.innerText = i + '%';
+const tmpFunc = function() {
+    setTimeout(() => {
+        start();
     }, 1000);
+}
+
+const start = function() {
+    if (currentIndex === 0) {
+        imgLeft.className = "cache";
+    }
+    else if ((currentIndex + 1) === data.length) {
+        imgRight.className = "cache";
+    }
+    else {
+        imgLeft.className = "";
+        imgRight.className = "";
+    }
+    clearInterval(timerProgress);
+    timerProgress = undefined;
+
+    const currentProjet = data[currentIndex];
+    changeProjet(currentProjet);
+    initTimer(currentProjet.duree);
+}
+
+const changeProjet = function(projet) {
+    projetElem.innerText = projet.projet;
+    personnesElem.innerText = projet.personnes;
+}
+
+const initTimer = function(secondes) {
+    let i = 0;
+    let val = 1 / secondes * 100;
+    let currentVal = 0;
+
+    if (secondes === 0) {
+        time.innerText = "00:00";
+        progress.style.width = '100%';
+        progress.innerText ='100%';
+    }
+    else {
+        time.innerText = ("" + Math.floor(((secondes - i) / 60))).padStart(2, "0") + ":" + ("" + ((secondes - i) % 60)).padStart(2, "0");
+    
+        timerProgress = setInterval(function() {
+            i = i + 1;
+            currentVal = currentVal + val;
+            progress.style.width = Math.round(currentVal) + '%';
+            progress.innerText = Math.round(currentVal) + '%';
+    
+            time.innerText = ("" + Math.floor(((secondes - i) / 60))).padStart(2, "0") + ":" + ("" + ((secondes - i) % 60)).padStart(2, "0");
+            if (i === secondes) {
+                clearInterval(timerProgress);
+                timerProgress = undefined;
+            }
+        }, 1000);
+    }
 
 }
