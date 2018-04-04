34b5323fd473aaab97b1e1cae7351336fc55fab9
Sébastien Prévost <seprevost@airfrance.fr>
2018-04-04 15:03:33 +0200

    Ajout polyfill padstart

--- js/script.js
+++ js/script.js
@@ -81,3 +81,21 @@ const initTimer = function(secondes) {
     }
 
 }
+
+// Ployfill pour le PADSTART
+if (!String.prototype.padStart) {
+    String.prototype.padStart = function padStart(targetLength,padString) {
+        targetLength = targetLength>>0;
+        padString = String((typeof padString !== 'undefined' ? padString : ' '));
+        if (this.length > targetLength) {
+            return String(this);
+        }
+        else {
+            targetLength = targetLength-this.length;
+            if (targetLength > padString.length) {
+                padString += padString.repeat(targetLength/padString.length);
+            }
+            return padString.slice(0,targetLength) + String(this);
+        }
+    };
+}
