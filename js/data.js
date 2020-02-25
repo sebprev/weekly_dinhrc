b9ca4a7b97f9f80abd97db2eedb31ca8c8f21d02
Jean Lestang <jelestang@airfrance.fr>
2020-02-25 18:40:27 +0100

    Mise à jour février 2020

--- js/data.js
+++ js/data.js
@@ -1,109 +1,106 @@
 const milweb = {
     projet: 'MILWEB',
-    personnes: 'Joël',
-    images: 'Joel.jpg',
+    personnes: 'Joël C',
+    images: 'JoelC.png',
     duree: 180
 };
 
 const sirocco = {
     projet: 'SIROCCO',
     personnes: 'Jean',
-    images: 'Jean.jpg',
+    images: 'Jean.png',
     duree: 240
 };
 
 const siroccolight = {
     projet: 'SIROCCO LIGHT',
     personnes: 'Jean',
-    images: 'Jean.jpg',
-    duree: 120
-};
-
-const atcweb = {
-    projet: 'ATC (ATMOS)',
-    personnes: 'Philippe',
-    images: 'Philippe.jpg',
-    duree: 120
+    images: 'Jean.png',
+    duree: 60
 };
 
 const occtet = {
     projet: 'OCCTET',
     personnes: 'Jean',
-    images: 'Jean.jpg',
+    images: 'Jean.png',
+    duree: 60
+};
+
+const atmos = {
+    projet: 'ATMOS',
+    personnes: 'Philippe',
+    images: 'Philippe.png',
     duree: 60
 };
 
 const tfm = {
     projet: 'TFM',
-    personnes: 'Laurent',
-    images: 'Laurent.jpg',
+    personnes: 'Yoann',
+    images: 'Yoann.png',
     duree: 180
 };
 
 const ishare = {
     projet: 'ISHARE',
-    personnes: 'Laurent',
-    images: 'Laurent.jpg',
+    personnes: 'Yoann',
+    images: 'Yoann.png',
     duree: 120
 };
 
-const ptrakl = {
-    projet: 'PTRAKL',
-    personnes: 'Laurent',
-    images: 'Laurent.jpg',
+const occitane = {
+    projet: 'OCCITANE',
+    personnes: 'Olivier',
+    images: 'Olivier.png',
     duree: 60
 };
 
 const amigo = {
     projet: 'AMIGO',
     personnes: 'Corinne',
-    images: 'Corinne.jpg',
+    images: 'Corinne.png',
     duree: 120
 };
 
 const vidcco = {
     projet: 'VIDCCO',
     personnes: 'Corinne',
-    images: 'Corinne.jpg',
+    images: 'Corinne.png',
     duree: 60
 };
 
 const encours = {
     projet: 'Sujets transverses',
-    personnes: 'HCAP, Mohikan...',
+    personnes: 'Joël B',
+    images: 'JoelB.png',
     duree: 180
 };
 
 const archi = {
     projet: "Sujets d'architecture",
     personnes: 'Jean Marc',
-    images: 'JeanMarc.jpg',
+    images: 'JeanMarc.png',
     duree: 240
 };
 
 const technique = {
     projet: 'Sujets techniques',
-    personnes: 'Philippe, Laurent',
-    images: 'PhilippeEtLaurent.jpg',
+    personnes: 'Philippe, Yoann',
+    images: 'PhilippeEtYoann.png',
     duree: 240
 };
 
 const infos = {
     projet: "Infos générales",
     personnes: 'Stéphane',
+    images: 'Stephane.png',
     duree: 240
 };
 
 const demo = {
     projet: 'Démo de la semaine',
-    personnes: '???',
+    personnes: 'Weekly Hero',
+    images: 'weeklyHero.png',
     duree: 300
 };
 
-const nexttk = {
-    projet: 'Prochain Time Keeper',
-    personnes: '',
-    duree: 0
-};
-
-const data = [milweb, sirocco, siroccolight, atcweb, occtet, tfm, ishare, ptrakl, amigo, vidcco, encours, archi, technique, infos, nexttk, demo];
+const data = [milweb, sirocco, siroccolight, occtet, atmos, tfm, ishare, occitane, amigo, vidcco, encours, archi, technique, infos, demo];
